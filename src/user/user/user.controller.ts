aea400f64d503eb73ca3afd545536e288c014ec0
Your Name <jafalkaff96@gmail.com>
2026-09-02 07:53:43 +0700

    learn http request

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Post, Get } from '@nestjs/common';
+import { Controller, Post, Get, Req, Query, Param } from '@nestjs/common';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): string {
+    return `Hello ${firstName} ${lastName}`;
+  }
+
+  @Get('/:id')
+  getByid(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
   @Post()
   post(): string {
     return 'POST';
@@ -11,3 +24,15 @@ export class UserController {
     return 'Hello Nestjs';
   }
 }
+
+/**
+ * Http request list
+ * 1. @Req() for express.request
+ * 2. @Param(key?) for req.params.key
+ * 3. $Body(key?) for req.body.key
+ * 4. @Query(key?) for req.query.key
+ * 5. @Header(key?) for req.headers.key
+ * 6. @Ip() for req.ip
+ * 7. @HostParam() for req.hosts
+ *
+ */
